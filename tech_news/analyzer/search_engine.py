85d7f7520555bce1a65fd4821ebc596bde7cc206
Thedywender <thedywender@hotmail.com>
2024-05-24 15:41:14 -0400

    req 07 ok

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,7 +1,13 @@
+from tech_news.database import db
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    news_return_data = db.news.find(
+        {"title": {"$regex": title, "$options": "i"}},
+        {"title": 1, "url": 1, "_id": 0},
+    )
+    return [(new["title"], new["url"]) for new in news_return_data]
 
 
 # Requisito 8
