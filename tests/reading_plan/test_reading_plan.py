1dd2db5e53143a827e78c02ea70bda10c9100cc6
Thedywender <thedywender@hotmail.com>
2024-05-24 15:15:42 -0400

    req 06 ok

--- tests/reading_plan/test_reading_plan.py
+++ tests/reading_plan/test_reading_plan.py
@@ -1,5 +1,30 @@
-from tech_news.analyzer.reading_plan import ReadingPlanService  # noqa: F401, E261, E501
+from unittest.mock import patch
+from tech_news.analyzer.reading_plan import ReadingPlanService
+import pytest
+
+news_data = [
+    {"title": "Python", "reading_time": 4},
+    {"title": "Pytest", "reading_time": 3},
+    {"title": "Parsel", "reading_time": 10},
+    {"title": "Beautiful Soup", "reading_time": 15},
+    {"title": "Requests", "reading_time": 20},
+]
+
+expected_results = {
+    "readable": [
+        {"unfilled_time": 8, "chosen_news": [("Python", 4), ("Pytest", 3)]},
+        {"unfilled_time": 5, "chosen_news": [("Parsel", 10)]},
+        {"unfilled_time": 0, "chosen_news": [("Beautiful Soup", 15)]},
+    ],
+    "unreadable": [("Requests", 20)],
+}
 
 
 def test_reading_plan_group_news():
-    pass
+    ReadingPlanService._db_news_proxy = lambda: news_data
+
+    with pytest.raises(ValueError):
+        ReadingPlanService.group_news_for_available_time(0)
+
+    result = ReadingPlanService.group_news_for_available_time(15)
+    assert result == expected_results
