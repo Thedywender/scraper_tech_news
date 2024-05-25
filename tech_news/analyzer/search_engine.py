2be9c35533757d2ca5c3d07a6c4453b535dbe4a9
Thedywender <thedywender@hotmail.com>
2024-05-25 12:52:04 -0400

    req 08 e 09 ok

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import db
+from datetime import datetime
 
 
 # Requisito 7
@@ -7,16 +8,33 @@ def search_by_title(title):
         {"title": {"$regex": title, "$options": "i"}},
         {"title": 1, "url": 1, "_id": 0},
     )
-    return [(new["title"], new["url"]) for new in news_return_data]
+    news_data = [(new["title"], new["url"]) for new in news_return_data]
+    return news_data
 
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    date = datetime.strptime(date, "%Y-%m-%d").strftime("%d/%m/%Y")
+
+    news_return_data = db.news.find(
+        {"timestamp": {"$regex": date, "$options": "i"}},
+        {"title": 1, "url": 1, "_id": 0},
+    )
+    news_data = [(new["title"], new["url"]) for new in news_return_data]
+    return news_data
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    news_return_data = db.news.find(
+        {"category": {"$regex": category, "$options": "i"}},
+        {"title": 1, "url": 1, "_id": 0},
+    )
+
+    news_data = [(new["title"], new["url"]) for new in news_return_data]
+    return news_data
