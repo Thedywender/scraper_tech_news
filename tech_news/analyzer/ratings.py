5bf04244818143a4dc12ace5f4ab440141b91330
Thedywender <thedywender@hotmail.com>
2024-05-27 16:42:21 -0400

    finished req10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,12 @@
-# Requisito 10
+from collections import Counter
+from tech_news.database import db
+
+
 def top_5_categories():
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    news_data = db.news.find({}, {"category": 1, "_id": 0})
+    categories = [new["category"] for new in news_data if "category" in new]
+    category_counts = Counter(categories)
+    most_common_categories = category_counts.most_common(5)
+    most_common_categories.sort(key=lambda x: (-x[1], x[0]))
+
+    return [category for category, count in most_common_categories]
